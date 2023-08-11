8a59c3fa1c474752fd9efaef7bed25089e47cbb9
CrimsonCoder42 <nostro37@gmail.com>
2023-08-11 10:16:43 -0400

    1:06:36

---
+++ hello.go
@@ -0,0 +1,7 @@
+package main
+
+import testpackage "gotutorial/test"
+
+func main() {
+	testpackage.MyFunction()
+}
---
+++ test/test.go
@@ -0,0 +1,10 @@
+package testpackage
+
+import "fmt"
+
+func MyFunction() {
+	fmt.Println("step 1")
+	fmt.Println("step 2")
+	fmt.Println("step 3")
+	fmt.Println("step 4")
+}
